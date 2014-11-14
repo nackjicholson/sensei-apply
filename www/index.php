0b652c2bee497011f6d8221fcdb14e7d8e8a72fc
Will Vaughn <willieviseoae@gmail.com>
2014-11-14 10:04:30 -0800

    basic version.

--- www/index.php
+++ www/index.php
@@ -1,11 +1,13 @@
 <?php
 
 use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\ResourceNotFoundException;
 use Aws\S3\S3Client;
 use Monolog\Logger;
 use Nack\FileParser\FileParser;
 use Nack\Monolog\Handler\GitterImHandler;
 use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,6 +16,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Application();
 
 $app['debug'] = true;
+
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views',
+));
 
 $app['config'] = $app->share(function() {
     $fileParser = new FileParser();
@@ -39,12 +45,40 @@ $app['logger'] = $app->share(function() use ($app) {
     return $logger;
 });
 
-$app->post('/api/v1/apply', function(Request $request) use ($app) {
+$app->get('/', function() use ($app) {
+
     $bucket = $app['config']['resumesBucket'];
-    $region = $app['config']['region'];
 
     /** @var S3Client $s3 */
     $s3 = $app['s3'];
+
+    $iterator = $s3->getIterator('ListObjects', array(
+        'Bucket' => $bucket
+    ));
+
+    $fileList = [];
+    foreach ($iterator as $object) {
+        $fileList[] =  $object['Key'] . "\n";
+    }
+
+    return $app['twig']->render('home.twig', array(
+        'fileList' => $fileList,
+    ));
+
+});
+
+$app->post('/api/v1/apply', function(Request $request) use ($app) {
+    $bucket = $app['config']['resumesBucket'];
+    $tableName = $app['config']['applicantsTable'];
+    $region = $app['config']['region'];
+
+    /** @var Logger $logger */
+    $logger = $app['logger'];
+
+    /** @var S3Client $s3 */
+    $s3 = $app['s3'];
+
+    /** @var DynamoDbClient $dynamoDb */
     $dynamoDb = $app['dynamoDb'];
 
     if (!$s3->doesBucketExist($bucket)) {
@@ -52,22 +86,52 @@ $app->post('/api/v1/apply', function(Request $request) use ($app) {
         $s3->waitUntil('BucketExists', ['Bucket' => $bucket]);
     }
 
+    try {
+        $dynamoDb->describeTable(['TableName' => $tableName]);
+    } catch (ResourceNotFoundException $resourceNotFoundException) {
+        $dynamoDb->createTable([
+            'TableName' => $tableName,
+            'AttributeDefinitions' => [
+                [
+                    'AttributeName' => 'name',
+                    'AttributeType' => 'S'
+                ]
+            ],
+            'KeySchema' => [
+                [
+                    'AttributeName' => 'name',
+                    'KeyType'       => 'HASH'
+                ]
+            ],
+            'ProvisionedThroughput' => [
+                'ReadCapacityUnits'  => 1,
+                'WriteCapacityUnits' => 1
+            ]
+        ]);
+    }
+
     /** @var UploadedFile $resumeFile */
     $resumeFile = $request->files->get('resume');
+    $key = $resumeFile->getClientOriginalName();
+    $s3->upload($bucket, $key, fopen($resumeFile->getPathname(), 'r+'));
 
-    $s3->upload($bucket, $resumeFile->getClientOriginalName(), fopen($resumeFile->getPathname(), 'r+'));
+    $profile = $request->request->get('profile');
+    $name = $profile['name'];
 
+    $dynamoDb->putItem([
+        'TableName' => $tableName,
+        'Item' => $dynamoDb->formatAttributes([
+            'name' => $name,
+            'visible' => true,
+            'bucket' =>  $bucket,
+            'key' => $key
+        ])
+    ]);
 
-//    $dynamoDb->createBucket(
-//        ['Bucket' => $bucket, '@future' => true]
-//    )
+    $link = $app['config']['homeLink'];
+    $logger->notice("New resume received for $name, check $link");
 
-//    $profile = $request->request->get('profile');
-
-//    var_dump($request->request);
-//    var_dump($request->files);
-
-    return $app->json(true);
+    return $app->json("Thank you for applying via api {$name}!");
 });
 
 $app->run();

1a1df26caddd6c3a0bcaa3ace31fd4087c2041e7
Will Vaughn <willieviseoae@gmail.com>
2015-05-11 23:03:06 -0700

    provisioning works.

--- support/ec2_setup.sh
+++ support/ec2_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+# Install nvm, iojs, pm2
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install iojs
 npm install -g pm2
 
+# Run server as daemon
 cd /var/cascade/sensei-apply
 npm install --production
 pm2 start es6/server.js --next-gen-js -i 2
--- support/nvm_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.1/install.sh | bash

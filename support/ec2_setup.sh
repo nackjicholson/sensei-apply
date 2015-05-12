ec155b451520a83194a129db21afd5fe2c379328
Will Vaughn <willieviseoae@gmail.com>
2015-05-11 22:42:22 -0700

    fiddling with shells

--- support/ec2_setup.sh
+++ support/ec2_setup.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -xe
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.1/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm install iojs
-npm install -g babel-node
+npm install -g babel
 
 cd /var/cascade/sensei-apply
 npm install --production
--- support/nvm_install.sh
+++ support/nvm_install.sh
@@ -1,2 +1,2 @@
-#!/bin/bash -xe
+#!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.1/install.sh | bash
